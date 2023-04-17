b7ea311edfedbfc577a250873ea0ed10d60a60b5
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-04-16 23:39:34 -0300

    feat: created a get route and unique email validation

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -17,13 +17,28 @@ export default async function handler(
   res: NextApiResponse
 ) {
   switch (req.method) {
+    case "GET":
+      const userEmail =
+        typeof req.query.userEmail === "string"
+          ? req.query.userEmail
+          : undefined;
+      if (!userEmail) {
+        res.status(400).send({ error: "Email não fornecido" });
+        return;
+      }
+      const userStocks = await getUserStocks(userEmail);
+      res.status(200).send(userStocks);
+      break;
+
     case "POST":
-      const { username, email, password } = req.body;
+      const { username, email, password, stocks } = req.body;
       try {
         validateName(username);
         validateEmail(email);
         validatePassword(password);
-        const user = await createUser(username, email, password);
+        const validateUser = await getUser(email);
+        if(validateUser) throw new Error('email já em uso')
+        const user = await createUser(username, email, password, stocks);
         createUserCollection(user);
         res.send("user successfully created");
       } catch (error: any) {
@@ -45,12 +60,14 @@ const encryptedPassword = async (password: string) => {
 const createUser = async (
   username: string,
   email: string,
-  password: string
+  password: string,
+  stocks: string
 ) => {
   const user = {
     username: username,
     email: email,
     password: await encryptedPassword(password),
+    stocks: stocks,
   };
   return user;
 };
@@ -62,6 +79,10 @@ const createUserCollection = async (user: User) => {
     const users = db.collection("users");
 
     const result = await users.insertOne({
+      username: user.username,
+      email: user.email,
+      password: user.password,
+      stocks: user.stocks,
       user,
     });
     console.log(`Inserted ${result.insertedId} document(s)`);
@@ -89,4 +110,41 @@ const validateName = (name: string) => {
   const nameRegex = /^\s*[a-zA-Z]{4,25}\s*$/;
   if (nameRegex.test(name)) return true;
   else throw new Error("invalid name");
+};
+
+const getUserStocks = async (email: string) => {
+  try {
+    await client.connect();
+    const db = client.db("InvestmentViewer");
+    const users = db.collection("users");
+
+    const user = await users.findOne({ email });
+
+    if (!user) {
+      throw new Error("Usuário não encontrado");
+    }
+
+    return user.stocks;
+  } catch (err) {
+    console.error(err);
+    throw new Error("Erro ao recuperar os stocks do usuário");
+  } finally {
+    await client.close();
+  }
+};
+
+const getUser = async (email: string) => {
+  try {
+    await client.connect();
+    const db = client.db("InvestmentViewer");
+    const users = db.collection("users");
+
+    const user = await users.findOne({ email });
+    return user;
+  } catch (err) {
+    console.error(err);
+    throw new Error("Erro ao validar email");
+  } finally {
+    await client.close();
+  }
 };
